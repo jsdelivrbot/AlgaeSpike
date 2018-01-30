be150ca5b148ac61528a28c5e20836e1d089db3d
Wallace <wallaceja@msoe.edu>
2018-01-30 08:51:18 -0600

    Added the Dashboard page.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
 import { DatabaseworkComponent } from './databaseTest/databasework/databasework.component';
-
+import {DashboardComponent} from './Lakes/pages/dashboard/dashboard.component';
+import {AppRoutingModule} from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DatabaseworkComponent
+    DatabaseworkComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
