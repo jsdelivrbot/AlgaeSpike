44fea94694f0c755c4bfa8e9d03d5e64b9b2e533
walesaj <walesaj@msoe.edu>
2018-01-30 09:11:34 -0600

    buoyDataComponent added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 
 import {DashboardComponent} from './Lakes/pages/dashboard/dashboard.component';
+import {BuoyDataComponent} from './Lakes/pages/buoyData/buoyData.component';
 
 const routes: Routes = [
-    {path: '', component: DashboardComponent}
+    {path: '', component: DashboardComponent},
+    {path: '/api/upload_data', component: BuoyDataComponent}
 ];
 
 @NgModule({
