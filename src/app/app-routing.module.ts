f0b0a1bf211b8b230541cc9adf81c07cf8c41a67
Kay Thao <thaokaye@msoe.edu>
2018-01-30 09:31:25 -0600

    removed uneeded files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,11 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 
 import {DashboardComponent} from './Lakes/pages/dashboard/dashboard.component';
-import {BuoyDataComponent} from './Lakes/pages/buoyData/buoyData.component';
 
 import {ProfileComponent} from './Lakes/pages/profile/profile.component';
 
 const routes: Routes = [
     {path: '', component: ProfileComponent},
-    {path: '/api/upload_data', component: BuoyDataComponent},
     {path: 'dashboard', component: DashboardComponent}
 ];
 
